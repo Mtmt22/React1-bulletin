1bf712b171f51485d5af97ae1db6116b20ce4869
ymatsumo <my18yolo7ty@gmail.com>
2025-01-26 18:19:38 +0900

    Create thread list screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+import { useState, useEffect} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]); //データを保存するstate
+
+  useEffect(() => {
+    //データを取得する
+    fetch("https://railway.bulletinboard.techtrain.dev/threads") //APIのURL
+    .then(response => response.json()) //json形式で取得
+    .then(data => setData(data)) //取得したデータをstateに保存
+    .catch(error => console.error("エラーが発生しました",error)); //エラーが発生した場合
+  }, []); //初回のみ実行
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>Happy hacking!!!</p>
+      <Header />
+      <h1 className='shinchaku'>新着スレッド</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item.title}</li>
+          ))}
+      </ul>
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>掲示板</h1>
+    </div>
+  );
+}
+
+export default Header;
